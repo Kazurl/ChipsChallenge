f19a5752ee27bad713016fe69362634ac57b7dd2
EnriqueCO1 <150913597+EnriqueCO1@users.noreply.github.com>
2023-11-20 10:00:08 +0000

    Create Interactables.java

---
+++ Interactables.java
@@ -0,0 +1,14 @@
+public class Interactables {
+
+    Player player = new Player();
+    Mobs mob = new Mobs(Tile tile);
+
+
+    public void interactMob(Mobs mob){
+        this.mob = mob;
+    }
+
+    public void interactPlayer(Player player){
+    this.player = player;
+    }
+}
