ee0ce776e1a2791b847d7e87fe3518df19039f6f
EnriqueCO1 <150913597+EnriqueCO1@users.noreply.github.com>
2023-11-24 16:06:28 +0000

    Created Item class

---
+++ Item.java
@@ -0,0 +1,22 @@
+public class Item{
+
+    public int itemLocationX, itemLocationY;
+
+
+    public void setX(int locationX) {
+        this.itemLocationX = locationX;
+    }
+
+    public void setY(int locationY) {
+        this.itemLocationY = locationY;
+    }
+
+    public int getX() {
+        return this.itemLocationX;
+    }
+
+    public int getY(int locationY) {
+        return this.itemLocationY;
+    }
+    
+}
