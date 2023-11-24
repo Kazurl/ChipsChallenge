6aa271a304510f5461e28eddb396f748a06fda10
WanTanHoe <148073879+WanTanHoe@users.noreply.github.com>
2023-11-24 22:09:08 +0000

    Merged Methods from Chip to ComputerChip

--- ComputerChip.java
+++ ComputerChip.java
@@ -1,5 +1,12 @@
 public class ComputerChip extends Item {
 
+    private int numOfChips;
+
+    public void interactPlayer(Player player){
+        player.getInventory();
+        numOfChips = numOfChips +1;
+        player.addToInventory(numOfChips);
+    }
     public boolean hasPlayerContact() {
         // Implement logic to detect player contact with computer chip
         // Return true if contact occurred, false otherwise
