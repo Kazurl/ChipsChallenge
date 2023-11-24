1a838191de2683525b822307a4a3270b6c59d89e
WanTanHoe <148073879+WanTanHoe@users.noreply.github.com>
2023-11-24 19:02:37 +0000

    Update miscellaneous

--- Interactables.java
+++ Interactables.java
@@ -3,7 +3,7 @@ public abstract class Interactables {
     protected Player player;
     protected Mob mob;
 
-    public abstract void interactMob(Mobs mob); // Implement them in the relevant classes
+    public abstract void interactMob(Mob mob); // Implement them in the relevant classes
 
     public abstract void interactPlayer(Player player); // Implement them in the relevant classes
 }
