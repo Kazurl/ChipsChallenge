15c35458b8ea6b766822928b2767f90512581918
EnriqueCO1 <150913597+EnriqueCO1@users.noreply.github.com>
2023-11-26 12:35:21 +0000

    Block class

---
+++ Block.java
@@ -0,0 +1,14 @@
+public class Block{
+
+    private boolean onIce;
+    private boolean collision = true;
+    private int locationX;
+    private int locationY;
+
+    public void slideOnIce(Tile tile){
+    }
+
+    public void interactPlayer(Player player){
+
+    }
+}
