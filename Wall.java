139b8c5a3c629f455049eabb7ba185bd28dac78c
EnriqueCO1 <150913597+EnriqueCO1@users.noreply.github.com>
2023-12-06 14:59:01 +0000

    Javadoc Wall.java

--- Wall.java
+++ Wall.java
@@ -1,8 +1,14 @@
+/**
+ * The Wall class represents a tile of type Wall, extending the Tile class.
+ * It is a non-walkable tile that blocks movement.
+ */
 public class Wall extends Tile {
 
-    //Constructor
+    /**
+     * Constructor for the Wall class.
+     * Creates a non-walkable wall tile.
+     */
     public Wall() {
         super(false, false);
     }
-
 }
