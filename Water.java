96299a99bd8d0b1b9231ff56dc27db343f4e49f9
EnriqueCO1 <150913597+EnriqueCO1@users.noreply.github.com>
2023-12-06 14:56:11 +0000

    Javadoc Water.java

--- Water.java
+++ Water.java
@@ -1,27 +1,49 @@
+/**
+ * The Water class represents a tile of type Water, extending the Tile class.
+ * It contains information about walkability and events triggered when a player walks on it.
+ */
 public class Water extends Tile {
-  //Constructor
-  public Water(boolean convertPath) {
-    super(convertPath, true);
-  }
-  public Boolean onPlayerWalk; {
-  }//PLACEHOLDER FOR BLOCK CLASS  
-  public void setWalkable(boolean walk) {
-    super.setWalkable(walk);
-    if (walk == true) {
-      onPlayerWalk = true;
-    } else {
-      onPlayerWalk = false;
+
+    /**
+     * Constructor for the Water class.
+     *
+     * @param convertPath True if the tile should convert to a path, false otherwise.
+     */
+    public Water(boolean convertPath) {
+        super(convertPath, true);
     }
-    /*
-    public Mobs onMobWalk;
-    {
-      super.setWalkable(walk);
-      if (walk == true) {
-        onMobWalk = false;
-      } else {
-        onMobWalk = true;
-      }
+
+    /**
+     * Placeholder for handling events when a player walks on the Water tile.
+     */
+    public Boolean onPlayerWalk; // PLACEHOLDER FOR BLOCK CLASS
+
+    /**
+     * Sets the walkability of the Water tile and handles events when a player walks on it.
+     *
+     * @param walk True if the tile should be walkable, false otherwise.
+     */
+    public void setWalkable(boolean walk) {
+        super.setWalkable(walk);
+        if (walk) {
+            onPlayerWalk = true;
+        } else {
+            onPlayerWalk = false;
+        }
+
+        /*
+         * Placeholder for handling events when a mob walks on the Water tile.
+         */
+        /*
+        public Mobs onMobWalk;
+        {
+          super.setWalkable(walk);
+          if (walk == true) {
+            onMobWalk = false;
+          } else {
+            onMobWalk = true;
+          }
+        }
+        */
     }
-    */
-  }
 }
