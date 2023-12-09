5928fe9be4396195325ba492d3f881b009f15878
FredHinds <2213595@swansea.ac.uk>
2023-12-09 12:20:17 +0000

    removed some commented out code

--- ComputerChip.java
+++ ComputerChip.java
@@ -1,19 +1,9 @@
 public class ComputerChip extends Item {
-
-    //From Freddie: the number of chips will be saved in the inventory, not here
-    //private int numOfChips;
-
     public ComputerChip(int itemLocationX, int itemLocationY) {
         super(itemLocationX, itemLocationY);
     }
-    //From Freddie: I have edited this
     public void interactPlayer(Player player){
         player.addToInventory("chip");
     }
-    /*
-    public boolean hasPlayerContact() {
-        // Implement logic to detect player contact with computer chip
-        // Return true if contact occurred, false otherwise
-    }
-    */
+
 }
--- Dirt.java
+++ Dirt.java
@@ -4,28 +4,5 @@ public class Dirt extends Tile {
     public Dirt(boolean convertPath) {
       super(convertPath, false);
     }
-/* From Freddie: Not working at the moment
-    public Player onPlayerWalk; {
-    }
 
-    public void setWalkable(boolean walk) {
-      super.setWalkable(walk);
-    if (walk == true) {
-      onPlayerWalk = true;
-
-    }
-    else {
-      onPlayerWalk = false;
-    }
-    public Mobs onMobWalk; {
-      super.setWalkable(walk);
-      if (walk == true) {
-        onMobWalk = false;
-      }
-      else {
-        onMobWalk = true;
-      }
-    }
-  }
-  */
 }
