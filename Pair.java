a6f6313054d4617cd1a44f405d069093900c2fa2
EnriqueCO1 <150913597+EnriqueCO1@users.noreply.github.com>
2023-12-10 18:47:27 +0000

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -1,5 +1,13 @@
 public class Pair {
-    int Item1, Item2;
+    int Item1;
+    int Item2;
+
+    /**
+     * Constructs a new Pair with the specified values for the first and second integers.
+     * 
+     * @param f The value for the first integer (Item1).
+     * @param s The value for the second integer (Item2).
+     */
     public Pair(int f, int s)
     {
         this.Item1 = f;
